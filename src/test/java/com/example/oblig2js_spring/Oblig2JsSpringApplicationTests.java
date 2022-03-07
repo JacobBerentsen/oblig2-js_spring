982b9806e1322ecd40a5ae020697aae2de11c18e
JacobBerentsen <91196338+JacobBerentsen@users.noreply.github.com>
2022-03-07 18:08:08 +0100

    Initial commit

---
+++ src/test/java/com/example/oblig2js_spring/Oblig2JsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oblig2js_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig2JsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
